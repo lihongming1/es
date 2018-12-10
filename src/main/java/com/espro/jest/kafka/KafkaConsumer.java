68179c9780d7c60d445d7d1959961f442bf8d5b2
lihongming <lihongming@dankegongyu.com>
2018-12-10 08:33:34 +0800

    search 功能

--- src/main/java/com/espro/jest/kafka/KafkaConsumer.java
+++ src/main/java/com/espro/jest/kafka/KafkaConsumer.java
@@ -9,9 +9,8 @@ public class KafkaConsumer {
      * 监听seckill主题,有消息就读取
      * @param message
      */
-    @KafkaListener(topics = {"test_es"})
+    @KafkaListener(topics = {"example"})
     public void receiveMessage(String message){
-        //收到通道的消息之后执行秒杀操作
 
         System.out.println(message);
 
